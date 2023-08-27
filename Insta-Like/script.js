25e4628b44bec31dae7d985f22bdf9b5c838da53
Tushar Seth <tusharrealme@gmail.com>
2023-08-27 10:14:51 +0530

    Add Project 2

---
+++ Insta-Like/script.js
@@ -0,0 +1,17 @@
+let con = document.querySelector("#container");
+let love = document.querySelector("i");
+
+con.addEventListener("dblclick",function(){
+    love.style.transform = 'translate(-50%,-50%) scale(1)'
+    love.style.color = "red";
+    love.style.opacity = 0.8;
+    setTimeout(() => {
+       love.style.opacity = 0;
+        
+    }, 1000);
+    setTimeout(() => {
+        love.style.transform = 'translate(-50%,-50%) scale(0)'
+        
+    }, 2000);
+})
+
